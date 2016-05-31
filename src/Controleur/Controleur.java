a2c71bcae8ece83214d3383bba185daae1ecb49d
TMichaudel <thibaud.michaudel@gmail.com>
2016-05-31 22:56:52 +0200

    machines

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -1,5 +1,12 @@
 package Controleur;
 
+import Vue.Vue;
+import Vue.VueAleatoire;
+import Vue.VueManuel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +17,25 @@ package Controleur;
  *
  * @author Thibaud
  */
-public class Controleur {
+public class Controleur implements ActionListener {
+    Vue vue;
+    public Controleur (Vue vue) {
+        this.vue=vue;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String c = e.getActionCommand();
+        if (c.equals("Manuela, la tortuga")) {
+            VueManuel fenetre = new VueManuel();
+            fenetre.setVisible(true);
+        }
+        
+        else if (c.equals("Random, the turtles")) {
+            VueAleatoire fenetre = new VueAleatoire();
+            fenetre.setVisible(true);
+       }
+        vue.dispose();
+    }
     
 }
