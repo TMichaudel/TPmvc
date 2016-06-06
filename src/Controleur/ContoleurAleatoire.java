d223ca9f6eebd5c86740920bba8da04d9b877429
MathieuThouret <mathieu.thouret@gmail.com>
2016-06-06 09:34:33 +0200

    Merge origin/master
    Conflicts:
    	src/Controleur/ContoleurAleatoire.java

--- src/Controleur/ContoleurAleatoire.java
+++ src/Controleur/ContoleurAleatoire.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ThreadLocalRandom;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Thibaud
@@ -38,6 +38,7 @@ public class ContoleurAleatoire {
         }
 
         class next extends TimerTask {
+
             public void run() {
                 tick();
             }
@@ -48,24 +49,29 @@ public class ContoleurAleatoire {
 
     }
 
-    public void avancer(Tortue tortue, int v){
+    public void avancer(Tortue tortue, int v) {
         int posX = tortue.getPosX();
         int posY = tortue.getPosY();
         int dir = tortue.getDir();
         Dimension size = this.vue.getFeuille().getSize();
-        int newX = (int) Math.round(posX + v * Math.cos(dir));
-        int newY = (int) Math.round(posY + v * Math.sin(dir));
-        /*if (newX > 0 && newY > 0 && newX < size.width && newY < size.height){
-            tortue.avancer(v);
+        int newX = (int) Math.round(posX + v * Math.cos(dir*0.0174533));
+        int newY = (int) Math.round(posY + v * Math.sin(dir*0.0174533));
+
+        tortue.avancer(v);
+        if (newX < 0) {
+            tortue.setPosition(size.width + newX, newY);
         }
-        else {*/
-            tortue.avancer(v);
-            if (newX < 0) tortue.setPosition(size.width + newX, newY);
-            if (newY < 0) tortue.setPosition(newX, newY + size.height);
-            if (newX > size.width) tortue.setPosition(newX-size.width, newY);
-            if (newY > size.height) tortue.setPosition(newX, newY-size.height);
+        if (newY < 0) {
+            tortue.setPosition(newX, newY + size.height);
+        }
+        if (newX > size.width) {
+            tortue.setPosition(newX - size.width, newY);
+        }
+        if (newY > size.height) {
+            tortue.setPosition(newX, newY - size.height);
+        }
     }
-    
+
     public void tick() {
         for (Tortue tortue : tortues) {
             tortue.setDirection(ThreadLocalRandom.current().nextInt(0, 360));
--- src/Controleur/ControleurFlock.java
+++ src/Controleur/ControleurFlock.java
@@ -1,8 +1,10 @@
 package Controleur;
 
+import Modele.Tortue;
 import Modele.TortueIntelligente;
 import Vue.VueFlock;
 import Vue.VueSuperTortue;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,11 +20,11 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author Thibaud
  */
 public class ControleurFlock {
-    
+
     private static int NB_TORTUES = 10;
     private ArrayList<TortueIntelligente> tortues;
     private VueFlock vue;
-    
+
     public ControleurFlock(VueFlock vue) {
         TortueIntelligente tortue;
         this.vue = vue;
@@ -34,23 +36,46 @@ public class ControleurFlock {
             this.vue.getFeuille().addTortue(new VueSuperTortue(tortue));
             tortue.addObserver(this.vue);
         }
-        
+
         class next extends TimerTask {
-            
+
             public void run() {
                 tick();
             }
         }
-        
+
         Timer timer = new Timer();
         timer.schedule(new next(), 0, 500);
-        
+
     }
-    
+
+    public void avancer(Tortue tortue, int v) {
+        int posX = tortue.getPosX();
+        int posY = tortue.getPosY();
+        int dir = tortue.getDir();
+        Dimension size = this.vue.getFeuille().getSize();
+        int newX = (int) Math.round(posX + v * Math.cos(dir*0.0174533));
+        int newY = (int) Math.round(posY + v * Math.sin(dir*0.0174533));
+        
+        tortue.avancer(v);
+        if (newX < 0) {
+            tortue.setPosition(size.width + newX, newY);
+        }
+        if (newY < 0) {
+            tortue.setPosition(newX, newY + size.height);
+        }
+        if (newX > size.width) {
+            tortue.setPosition(newX - size.width, newY);
+        }
+        if (newY > size.height) {
+            tortue.setPosition(newX, newY - size.height);
+        }
+    }
+
     public void tick() {
         for (TortueIntelligente tortue : tortues) {
             tortue.regarderEnvirons(tortues);
-            tortue.avancer(tortue.getVitesse());
+            avancer(tortue, tortue.getVitesse());
         }
     }
 }
